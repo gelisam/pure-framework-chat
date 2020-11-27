e544d27aeacc227a2464900c10c95be3055b90b9
Samuel Gélineau <gelisam+github@gmail.com>
2020-11-26 23:02:02 -0500

    displayTUI, centered 'hello world'

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Exception
+import Graphics.Vty
 
-main :: IO ()
-main = putStrLn "typechecks."
+
+displayTUI
+  :: (DisplayRegion -> Image)
+  -> IO ()
+displayTUI mkPicture = do
+  out <- standardIOConfig >>= outputForConfig
+  bracket_ (reserveDisplay out) (releaseDisplay out) $ do
+    displayRegion <- displayBounds out
+    displayCtx <- displayContext out displayRegion
+    let picture = picForImage (mkPicture displayRegion)
+    outputPicture displayCtx picture
+    _ <- getLine
+    pure ()
+
+center
+  :: DisplayRegion
+  -> Image
+  -> Image
+center (ww, hh) img = translate ((ww - w) `div` 2)
+                                ((hh - h) `div` 2)
+                                img
+  where
+    w = imageWidth img
+    h = imageHeight img
+
+main
+  :: IO ()
+main = displayTUI $ \displayRegion
+  -> center displayRegion
+   $ string defAttr "hello world"
