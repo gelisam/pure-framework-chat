2e985e2c8137e467824b00c571948efd3916af4b
Samuel Gélineau <gelisam+github@gmail.com>
2020-11-30 23:43:19 -0500

    basic client-server

--- src/PureFramework/TUI.hs
+++ src/PureFramework/TUI.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, OverloadedStrings, RecordWildCards, ViewPatterns #-}
 module PureFramework.TUI
   ( module PureFramework.TUI
   , Key(..)
@@ -6,6 +6,10 @@ module PureFramework.TUI
 
 import Control.Monad
 import Data.Function
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitFailure)
+--import qualified Data.ByteString as ByteString
+import qualified Network.Simple.TCP as TCP
 
 import ImperativeVty
 
@@ -58,3 +62,33 @@ playTUI world0 mkTextPicture handleKey = withTerminal $ do
         pure ()
       Just world' -> do
         loop world'
+
+multiplayTUI
+  :: world
+  -> (world -> Int -> (Int, Int) -> TextPicture)
+  -> (world -> Int -> Key -> Maybe world)
+  -> IO ()
+multiplayTUI _world0 _mkTextPicture _handleKey = do
+  getArgs >>= \case
+    ["server", host, port] -> do
+      putStrLn "spinning server..."
+      TCP.listen (TCP.Host host) port $ \(acceptor, _) -> do
+        putStrLn "accepting connections..."
+        TCP.accept acceptor $ \(client, _) -> do
+          putStrLn "connected."
+          TCP.send client "hello from server"
+          msg <- TCP.recv client 1024
+          putStrLn $ "client says: " ++ show msg
+    ["client", host, port] -> do
+      putStrLn "connecting..."
+      TCP.connect host port $ \(server, _) -> do
+        putStrLn "connected."
+        TCP.send server "hello from client"
+        msg <- TCP.recv server 1024
+        putStrLn $ "server says: " ++ show msg
+    _ -> do
+      progName <- getProgName
+      putStrLn "usage:"
+      putStrLn $ "  " ++ progName ++ " server 0.0.0.0 PORT"
+      putStrLn $ "  " ++ progName ++ " client SERVER_IP PORT"
+      exitFailure
