62c24d28e7488e98f6d08c79ebc4c54e396183b3
Samuel Gélineau <gelisam+github@gmail.com>
2020-12-01 21:27:30 -0500

    [BROKEN] split playTUI into reusable pieces
    I'm going to have two sources of events, so I would like to split the
    current source of event from the logic which updates the screen.
    
    however, it doesn't look like I did this quite right, as the screen is
    updated with a one key delay, which is super annoying because you don't
    see the key you just pressed...

--- src/PureFramework/TUI.hs
+++ src/PureFramework/TUI.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE LambdaCase, OverloadedStrings, RecordWildCards, ViewPatterns #-}
+{-# LANGUAGE LambdaCase, OverloadedStrings, RecordWildCards, ScopedTypeVariables, ViewPatterns #-}
 module PureFramework.TUI
   ( module PureFramework.TUI
   , Key(..)
   ) where
 
+import Control.Concurrent.Async
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TMQueue
 import Control.Monad
 import Data.Function
 import System.Environment (getArgs, getProgName)
@@ -45,23 +48,64 @@ displayTUI mkTextPicture = withTerminal $ do
   drawTextPicture (mkTextPicture screenSize)
   void waitForKey
 
-playTUI
+-- A variant of playTUI in which the current world is controlled
+-- programmatically via a TMQueue instead of being controlled by the user's
+-- keypresses.
+roboPlayTUI
+  :: (world -> (Int, Int) -> TextPicture)
+  -> TMQueue world  -- input channel, close to quit
+  -> IO ()
+roboPlayTUI mkTextPicture worldQueue = do
+  screenSize <- getScreenSize
+  fix $ \loop -> do
+    atomically (readTMQueue worldQueue) >>= \case
+      Just world -> do
+        clearScreen
+        drawTextPicture (mkTextPicture world screenSize)
+        loop
+      Nothing -> do
+        -- quit
+        pure ()
+
+-- Emits Keys as they are pressed. More reusable than if it was emitting worlds.
+-- Intended to be spawned by withAsync.
+keyboardThread
+  :: TQueue Key  -- output channel
+  -> IO ()
+keyboardThread keyQueue = do
+  key <- waitForKey
+  atomically $ writeTQueue keyQueue key
+  keyboardThread keyQueue
+
+-- Receives Keys, emits worlds.
+-- Intended to be spawned by withAsync.
+keyHandlingThread
   :: world
+  -> (world -> Key -> Maybe world)
+  -> TQueue Key  -- input channel
+  -> TMQueue world  -- output channel, closed on Nothing
+  -> IO ()
+keyHandlingThread world handleKey keyQueue worldQueue = do
+  atomically $ writeTMQueue worldQueue world
+  key <- atomically $ readTQueue keyQueue
+  case handleKey world key of
+    Just world' -> do
+      keyHandlingThread world' handleKey keyQueue worldQueue
+    Nothing -> do
+      atomically $ closeTMQueue worldQueue
+
+playTUI
+  :: forall world
+   . world
   -> (world -> (Int, Int) -> TextPicture)
   -> (world -> Key -> Maybe world)
   -> IO ()
 playTUI world0 mkTextPicture handleKey = withTerminal $ do
-  screenSize <- getScreenSize
-  flip fix world0 $ \loop world -> do
-    clearScreen
-    drawTextPicture (mkTextPicture world screenSize)
-    key <- waitForKey
-    case handleKey world key of
-      Nothing -> do
-        -- quit
-        pure ()
-      Just world' -> do
-        loop world'
+  keyQueue <- newTQueueIO
+  worldQueue <- newTMQueueIO
+  withAsync (keyboardThread keyQueue) $ \_ -> do
+    withAsync (keyHandlingThread world0 handleKey keyQueue worldQueue) $ \_ -> do
+      roboPlayTUI mkTextPicture worldQueue
 
 multiplayTUI
   :: world
